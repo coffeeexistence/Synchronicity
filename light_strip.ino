35e753da1853fc1ee249a7c1124b66a87e358f98
John Anthony Rivera <coffeeexistence@users.noreply.github.com>
2019-05-26 15:56:30 -0700

    First proof-of-concept arduino code

--- light_strip.ino
+++ light_strip.ino
@@ -1,9 +1,166 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_NeoPixel.h>
 
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+#define pixel_count 150
+
+// Memory (SRAM) usage table:
+// 450 bytes: Adafruit_NeoPixel strip
+// 600 bytes: RGB currentState
+// 600 bytes: RGB destinationState
+// Total: 1,650 bytes
+// Uno total: 2048 bytes
+// Remaining: 398 bytes
+
+// 3 bytes SRAM per pixel
+
+// 24 byte Serial Strip Instruction
+// These will be read out from serial port one at a time
+// to prevent memory overflow
+struct StripInstruction
+{
+  uint8_t startPixel;
+  uint8_t endPixel;
+  uint8_t colorPreset;
+};
+
+struct RGB
+{
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+};
+
+enum EColorPresets
+{
+  COLOR_BLANK = 0,
+  COLOR_SOFT_RED = 1,
+};
+
+// Extraneous duplication here
+RGB colorPresets[2] = {
+    {0, 0, 0}, // 0 COLOR_BLANK
+    {6, 2, 2}  // 1 COLOR_SOFT_RED
+};
+
+// 3 bytes SRAM per RGB value
+RGB currentState[pixel_count];
+RGB destinationState[pixel_count];
+
+int loopDelay = 1;
+int interpolationRate = 8; // Every n loops
+uint16_t iterationCount = 0;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixel_count, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  StripInstruction instruction;
+  if (iterationCount == 1000)
+  {
+    iterationCount = 0;
+  }
 
+  if (iterationCount == 0)
+  {
+    instruction.startPixel = 0;
+    instruction.endPixel = 20;
+    instruction.colorPreset = COLOR_BLANK;
+    setDestinationStateFromStripInstruction(instruction);
+  }
+
+  if (iterationCount == 500)
+  {
+    instruction.startPixel = 0;
+    instruction.endPixel = 20;
+    instruction.colorPreset = COLOR_SOFT_RED;
+    setDestinationStateFromStripInstruction(instruction);
+  }
+
+  if (iterationCount % interpolationRate == 0)
+  {
+    interpolateCurrentStateTowardsDestinationState();
+    setStripToCurrentState();
+  }
+  iterationCount++;
+  delay(loopDelay);
+}
+
+void setDestinationStateFromStripInstruction(StripInstruction instruction)
+{
+  for (uint8_t i = instruction.startPixel; i <= instruction.endPixel; i++)
+  {
+    destinationState[i] = colorPresets[instruction.colorPreset];
+  }
+}
+
+// Not "true" interpolation, but much less memory intensive.
+void interpolateCurrentStateTowardsDestinationState()
+{
+  RGB currentRGBValue;
+  RGB destinationRGBValue;
+  for (uint8_t i = 0; i < strip.numPixels(); i++)
+  {
+    currentRGBValue = currentState[i];
+    destinationRGBValue = destinationState[i];
+
+    if (currentRGBValue.r != destinationRGBValue.r)
+    {
+      if (currentRGBValue.r < destinationRGBValue.r)
+      {
+        currentRGBValue.r++;
+      }
+      else
+      {
+        currentRGBValue.r--;
+      }
+    }
+
+    if (currentRGBValue.g != destinationRGBValue.g)
+    {
+      if (currentRGBValue.g < destinationRGBValue.g)
+      {
+        currentRGBValue.g++;
+      }
+      else
+      {
+        currentRGBValue.g--;
+      }
+    }
+
+    if (currentRGBValue.b != destinationRGBValue.b)
+    {
+      if (currentRGBValue.b < destinationRGBValue.b)
+      {
+        currentRGBValue.b++;
+      }
+      else
+      {
+        currentRGBValue.b--;
+      }
+    }
+    currentState[i] = currentRGBValue;
+  }
+}
+
+void setStripToCurrentState()
+{
+  for (uint8_t i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, currentState[i].r, currentState[i].g, currentState[i].b);
+  }
+  strip.show();
 }
